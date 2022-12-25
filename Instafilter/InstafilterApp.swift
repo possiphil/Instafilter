85a8be9e5689c879003944540c4d9081ba652956
Philipp Sanktjohanser <venture@mein.gmx>
2022-12-25 20:18:30 +0100

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Philipp Sanktjohanser on 25.12.22.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
